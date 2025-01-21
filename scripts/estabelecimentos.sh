b0f795c2000bb915eacd93751059d6d6ad2d7349
infobarbosa <infobarbosa@yahoo.com.br>
2025-01-21 07:28:30 -0300

    comentando linhas de importação de estabelecimentos no script de importação

--- scripts/estabelecimentos.sh
+++ scripts/estabelecimentos.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-python3 scripts/import_estabelecimentos.py /home/barbosa/labs/bases-empresas/extracted/Estabelecimentos0.csv 0
-python3 scripts/import_estabelecimentos.py /home/barbosa/labs/bases-empresas/extracted/Estabelecimentos1.csv 0
-python3 scripts/import_estabelecimentos.py /home/barbosa/labs/bases-empresas/extracted/Estabelecimentos2.csv 0
-python3 scripts/import_estabelecimentos.py /home/barbosa/labs/bases-empresas/extracted/Estabelecimentos3.csv 0
-python3 scripts/import_estabelecimentos.py /home/barbosa/labs/bases-empresas/extracted/Estabelecimentos4.csv 0
+# python3 scripts/import_estabelecimentos.py /home/barbosa/labs/bases-empresas/extracted/Estabelecimentos0.csv 0
+#python3 scripts/import_estabelecimentos.py /home/barbosa/labs/bases-empresas/extracted/Estabelecimentos1.csv 0
+#python3 scripts/import_estabelecimentos.py /home/barbosa/labs/bases-empresas/extracted/Estabelecimentos2.csv 0
+#python3 scripts/import_estabelecimentos.py /home/barbosa/labs/bases-empresas/extracted/Estabelecimentos3.csv 0
+#python3 scripts/import_estabelecimentos.py /home/barbosa/labs/bases-empresas/extracted/Estabelecimentos4.csv 0
 python3 scripts/import_estabelecimentos.py /home/barbosa/labs/bases-empresas/extracted/Estabelecimentos5.csv 0
 python3 scripts/import_estabelecimentos.py /home/barbosa/labs/bases-empresas/extracted/Estabelecimentos6.csv 0
 python3 scripts/import_estabelecimentos.py /home/barbosa/labs/bases-empresas/extracted/Estabelecimentos7.csv 0
